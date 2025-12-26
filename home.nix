81b3e20ae998d700821b50a6d19fc71d26c041c0
Andy Pandaan <andy.pandaan@outlook.com>
2025-12-26 12:09:54 +0400

    corrected state version. added aliases

--- home.nix
+++ home.nix
@@ -3,12 +3,14 @@
 {
   home.username = "andy";
   home.homeDirectory = "/home/andy";
-  home.stateVersion = "25.05";
+  home.stateVersion = "25.11";
   programs.git.enable = true;
   programs.bash = {
     enable = true;
     shellAliases = {
-      btw = "echo i use nixos, btw";
+      cat = "bat";
+      ls = "exa -l --icons";
+      k= "kubectl ";
     };
     profileExtra = ''
       if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
